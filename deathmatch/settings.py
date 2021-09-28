5f9417b56e1d1f7428f8b82fea9b6d9e7cbed803
thatcher thornberry <thatcherthornberry@gmail.com>
2021-09-27 19:48:36 -0500

    rm secrets

--- deathmatch/settings.py
+++ deathmatch/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=qubf2&s_xe+1vkdddv^^^y3f(12_2z!5^r5t%ekdd0zs)8@uz'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
